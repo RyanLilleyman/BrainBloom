930d84b05ba0550ae3078944a78b77564fb84bcd
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-01 15:34:43 -0400

    what

--- src/brainy-back/src/thoughts/thoughts.module.ts
+++ src/brainy-back/src/thoughts/thoughts.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ThoughtsController } from './thoughts.controller';
 
-@Module({})
+@Module({
+  controllers: [ThoughtsController]
+})
 export class ThoughtsModule {}
