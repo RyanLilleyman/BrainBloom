d3bd961e82fbaabc8387b50ca77c342964a1c942
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-01 15:37:04 -0400

    delete thouht modules

--- src/brainy-back/src/thoughts/thoughts.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ThoughtsController } from './thoughts.controller';
-
-@Module({
-  controllers: [ThoughtsController],
-})
-export class ThoughtsModule {}
