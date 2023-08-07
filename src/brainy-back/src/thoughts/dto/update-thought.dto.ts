5f38bf38d9282a759c33abe99062c05bb3f9c663
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-07 14:07:35 -0400

    Cleaned up

--- src/brainy-back/src/thoughts/dto/update-thought.dto.ts
+++ src/brainy-back/src/thoughts/dto/update-thought.dto.ts
@@ -3,7 +3,6 @@ import { CreateThoughtDto } from './create-thought.dto';
 
 export class UpdateThoughtDto extends PartialType(CreateThoughtDto) {
   title: string;
-  date: Date;
   content: string;
   status: CreateThoughtDto['status'];
 }
--- src/brainyFront/src/Features/Thoughts/EntryListView.tsx
+++ src/brainyFront/src/Features/Thoughts/EntryListView.tsx
@@ -55,7 +55,52 @@ const EntryListView: React.FC<EntryListViewProps> = ({
   const filteredEntries = entries.filter((entry) => {
     return entry.title.toLowerCase().includes(searchQuery.toLowerCase());
   });
+  // (value) => formik.setFieldValue("status", value)
 
+  const createEntry = (entry, index) => {
+    
+    const dateStr = entry.date.toLocaleString();
+    const dateObj = new Date(dateStr);
+
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1; // Months are zero-indexed.
+    const year = dateObj.getFullYear();
+
+    const hours = dateObj.getHours();
+    const minutes = dateObj.getMinutes().toString().padStart(2, "0"); // padStart ensures we always have 2 digits.
+    const seconds = dateObj.getSeconds().toString().padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return (
+      (
+        <View key={index} style={styles.entry}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{entry.title}</Text>
+          </View>
+          <Text style={styles.separator}></Text>
+          <Text style={styles.description}>{entry.content}</Text>
+          <Text style={styles.separator}></Text>
+          <View style={styles.statusContainer}>
+            <View style={styles.status}>
+              <Text style={styles.date}>Date:</Text>
+              <Text style={styles.date}>{formattedDate}</Text>
+            </View>
+            <View style={styles.status}>
+              <Text style={styles.date}>Type:</Text>
+              <Text style={styles.date}>{entry.status}</Text>
+            </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => onDelete(index)}
+          >
+            <Text style={styles.buttonText}>Delete</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      )
+    )
+  };
   return (
     <ScrollView
       contentContainerStyle={styles.scrollContainer}
@@ -64,41 +109,7 @@ const EntryListView: React.FC<EntryListViewProps> = ({
       {filteredEntries.length === 0 ? (
         <Text>No matching entries found.</Text>
       ) : (
-        filteredEntries.map((entry, index) => (
-          <View key={index} style={styles.entry}>
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>{entry.title}</Text>
-            </View>
-            <Text style={styles.separator}></Text>
-            <Text style={styles.date}>Date: {entry.date.toLocaleString()}</Text>
-            <Text style={styles.separator}></Text>
-            <Text style={styles.description}>{entry.content}</Text>
-            <Text style={styles.separator}></Text>
-            <View style={styles.statusContainer}>
-              <RNPickerSelect
-                onBlur={() => formik.setFieldTouched("status")}
-                placeholder={{ label: "Thought Pattern...", value: "" }}
-                onValueChange={(value) => formik.setFieldValue("status", value)}
-                items={[
-                  { label: "Worrying", value: ThoughtsStatus.WORRY },
-                  { label: "Ruminating", value: ThoughtsStatus.RUMINATE },
-                  { label: "Neutral", value: ThoughtsStatus.NEUTRAL },
-                ]}
-                style={{
-                  inputIOS: styles.pickerStyle,
-                  inputAndroid: styles.pickerStyle,
-                  inputWeb: styles.pickerStyle,
-                }}
-              />
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => onDelete(index)}
-              >
-                <Text style={styles.buttonText}>Delete</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        ))
+        filteredEntries.map((entry, index) => createEntry(entry, index))
       )}
     </ScrollView>
   );
@@ -148,6 +159,9 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     flex: 1,
+    fontSize: 24,
+    marginVertical: 5,
+    color: "#004d66",
   },
   date: {
     fontStyle: "italic",
@@ -157,6 +171,66 @@ const styles = StyleSheet.create({
   },
   description: {
     fontStyle: "italic",
+    width: "100%",
+    fontSize: 16,
+    color: "#333",
+    lineHeight: 20,
+    marginBottom: 5,
+    padding: 5,
+    borderRadius: 4,
+    backgroundColor: "rgba(230, 230, 230, 0.7)",
+  },
+  statusContainer: {
+    gap: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: 45,
+    borderBottomColor: "#DDD",
+    borderBottomWidth: 0.5,
+  },
+  status: {
+    margin: 5,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  statusText: {
+    fontSize: 16,
+    margin: 2,
+  },
+  button: {
+    alignSelf: "flex-end",
+    flex: 1,
+    backgroundColor: "#007B83",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 3,
+    padding: 8,
+    borderRadius: 5,
+    height: "90%",
+    maxWidth:"30%",
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 4,
+    // },
+    // shadowOpacity: 0.2,
+    // shadowRadius: 4,
+    // elevation: 8,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#F5F5F5",
+  },
+  separator: {
+    width: "100%",
+    marginVertical: 2,
+    color: "#DDD",
+    borderBottomColor: "#DDD",
+    borderBottomWidth: 0.5,
   },
 });
 
